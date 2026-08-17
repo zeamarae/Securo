ac86fec54895668c93e5d62b6f1be2e9ca18cdd3
John Paul Inso <john.paulinso@fusionos.ai>
2026-08-17 23:13:57 +0800

    Update Securo 08/17/2026 23:13:57.77

--- g.bat
+++ g.bat
@@ -5,6 +5,7 @@ if "%msg%"=="" set msg=Update Securo %date% %time%
 echo [1/3] Staging changes...
 git add .
 echo [2/3] Committing with message: %msg%
-git commit -m "%msg%"
+rem (2026-07-13) Allow empty commits; was git commit failing on clean tree
+git commit --allow-empty -m "%msg%"
 echo [3/3] Pushing to https://github.com/zeamarae/Securo.git ...
 git push origin main
