fd6b307e383e71488a9b464125b0e9c4546e60ef
zeamarae <zearamil9@gmail.com>
2026-09-07 23:26:14 +0800

    Update Securo 07/09/2026 23:26:14.37

--- g.bat
+++ g.bat
@@ -1,11 +1,12 @@
 @echo off
-rem (2026-07-13) Git commit & push automation to https://github.com/zeamarae/Securo.git
+rem (2026-09-07) Use full MinGit path; git not in PATH on this machine
+set GITEXE=%LOCALAPPDATA%\Programs\Git\cmd\git.exe
 set msg=%*
 if "%msg%"=="" set msg=Update Securo %date% %time%
 echo [1/3] Staging changes...
-git add .
+"%GITEXE%" add .
 echo [2/3] Committing with message: %msg%
 rem (2026-07-13) Allow empty commits; was git commit failing on clean tree
-git commit --allow-empty -m "%msg%"
+"%GITEXE%" commit --allow-empty -m "%msg%"
 echo [3/3] Pushing to https://github.com/zeamarae/Securo.git ...
-git push origin main
+"%GITEXE%" push origin main
